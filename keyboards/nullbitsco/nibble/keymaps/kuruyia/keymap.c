55ce67ff6665e0a8857ff0f05fda035f314de936
Kuruyia <8174691+kuruyia@users.noreply.github.com>
2021-08-07 18:58:06 +0200

    OLED now working

--- keyboards/nullbitsco/nibble/keymaps/kuruyia/keymap.c
+++ keyboards/nullbitsco/nibble/keymaps/kuruyia/keymap.c
@@ -16,6 +16,10 @@
 #include QMK_KEYBOARD_H
 #include "nibble_rgb_user.h"
 
+#define PROGRESSBAR_BORDER 0xFF
+#define PROGRESSBAR_EMPTY  0x81
+#define PROGRESSBAR_FULL   0x99
+
 enum layer_names { _MA, _FN };
 
 enum custom_keycodes {
@@ -32,9 +36,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_F16,  KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                    KC_RALT, MO(_FN), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
   ),
   [_FN] = LAYOUT_iso(
-               RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, KC_HOME,  KC_INS,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
-    KC_MNXT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+               RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, KC_HOME,  KC_END,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,           KC_INS,
+    KC_MNXT, KC_NLCK, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_VAI, _______,
     _______, _______, _______, _______,                            _______,                   _______, _______, RGB_TOG, KC_PRGB, RGB_VAD, KC_NRGB
   ),
@@ -93,3 +97,83 @@ void matrix_scan_user(void) {
     // Scan and parse keystrokes from remote keyboard, if connected (see readme)
     matrix_scan_remote_kb();
 }
+
+#ifdef OLED_DRIVER_ENABLE
+oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+    return OLED_ROTATION_90;
+}
+
+static inline void oled_write_raw_byte_at(const char data, const uint8_t x, const uint8_t y)
+{
+    oled_write_raw_byte(data, OLED_DISPLAY_HEIGHT * y + x);
+}
+
+static void draw_progress_bar(const uint8_t progress, const uint8_t y)
+{
+    // Draw the static parts
+    oled_write_raw_byte_at(PROGRESSBAR_BORDER, 0, y);
+    oled_write_raw_byte_at(PROGRESSBAR_EMPTY, 1, y);
+    oled_write_raw_byte_at(PROGRESSBAR_EMPTY, 2, y);
+
+    oled_write_raw_byte_at(PROGRESSBAR_EMPTY, OLED_DISPLAY_HEIGHT - 3, y);
+    oled_write_raw_byte_at(PROGRESSBAR_EMPTY, OLED_DISPLAY_HEIGHT - 2, y);
+    oled_write_raw_byte_at(PROGRESSBAR_BORDER, OLED_DISPLAY_HEIGHT - 1, y);
+
+    // Draw the bar
+    size_t i = 3;
+    const uint8_t endCol = progress * (OLED_DISPLAY_HEIGHT - 6) / UINT8_MAX + 3;
+
+    for (; i < endCol; ++i)
+    {
+        oled_write_raw_byte_at(PROGRESSBAR_FULL, i, y);
+    }
+
+    for (; i < OLED_DISPLAY_HEIGHT - 3; ++i)
+    {
+        oled_write_raw_byte_at(PROGRESSBAR_EMPTY, i, y);
+    }
+}
+
+void oled_task_user(void) {
+    // Get the current WPM
+    char wpmBuffer[4];
+    itoa(get_current_wpm(), wpmBuffer, 10);
+
+    // Show the WPM
+    oled_write_ln_P(PSTR("WPM"), false);
+    oled_write_ln(wpmBuffer, false);
+    oled_advance_page(false);
+
+    // Show the current layer
+    oled_write_P(PSTR("Layer"), false);
+    switch (get_highest_layer(layer_state)) {
+        case _MA:
+            oled_write_ln_P(PSTR("Main"), false);
+            break;
+        case _FN:
+            oled_write_ln_P(PSTR("FN"), false);
+            break;
+        default:
+            oled_write_ln_P(PSTR("Undefined"), false);
+    }
+
+    // Show RGB state
+    oled_set_cursor(0, 6);
+    oled_write_ln_P(PSTR("RGB"), false);
+
+    draw_progress_bar(rgblight_get_val(), 7);
+
+    // Show LED state
+    led_t led_state = host_keyboard_led_state();
+    oled_set_cursor(0, 15);
+
+    oled_write_P(PSTR("C"), led_state.caps_lock);
+    oled_advance_char();
+
+    oled_write_P(PSTR("N"), led_state.num_lock);
+    oled_advance_char();
+
+    oled_write_P(PSTR("S"), led_state.scroll_lock);
+    oled_advance_char();
+}
+#endif
--- keyboards/nullbitsco/nibble/keymaps/kuruyia/rules.mk
+++ keyboards/nullbitsco/nibble/keymaps/kuruyia/rules.mk
@@ -1,2 +1,6 @@
+# Enable more features
+OLED_DRIVER_ENABLE = yes
+WPM_ENABLE = yes
+
 # Add user source files
 SRC += nibble_rgb_user.c
